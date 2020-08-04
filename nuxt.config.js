1d79a5ccebaf87b82b135f3dcd8d76bba3e040d5
Kangxu Wang <kangxu@umich.edu>
2020-08-03 23:38:33 -0700

    Change port to 3000

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,4 +77,8 @@ export default {
    */
   build: {},
   serverMiddleware: ['~/server/index.js'],
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0', // default: localhost
+  },
 }
